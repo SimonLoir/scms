236ca31c6cf514d1e7e634675a17cf7a8846a1e9
SimonLoir <simon-loir@hotmail.com>
2017-04-15 22:06:04 +0200

    amélioration boutons + boutons de partage

---
+++ site/scms-modules/scms-social-medias/share-getter.js
@@ -0,0 +1,5 @@
+var x_result = {
+type: "scms-share-icons-div",
+module: "scms-social-medias"
+}
+x_result;
---
+++ site/scms-modules/scms-social-medias/share-html-code.js
@@ -0,0 +1,5 @@
+var div = document.createElement("div");
+$(div).addClass("scms-share-icons-div");
+$(div).html('Système de partage sur les réseaux sociaux');
+this.parentElement.insertBefore(div, this);
+$('#scms-get-json').click();
